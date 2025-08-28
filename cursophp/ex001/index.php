ffa7039acad0233d9c8883714afe7769017178d3
Heitor Berg <heitorberg114@gmail.com>
2025-08-28 08:35:38 -0300

    Add files via upload

---
+++ cursophp/ex001/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do servidor</title>
+</head>
+
+<body>
+    <h1>Dados do servidor</h1>
+    <p>Vamos ver os dados do servidor</p>
+    <?php
+    echo "<h2>Informações do PHP</h2>";
+    phpinfo();
+    ?>
+</body>
+
+</html>
